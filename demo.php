2fa25339e2442759fb4ea16622aedd8c9893f616
wangxiwei520 <826865439@qq.com>
2020-05-19 23:31:31 +0800

    composer

---
+++ demo.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2020/5/18
+ * Time: 23:13
+ */
+require_once 'vendor/autoload.php';
+use Abstarfish\Starfish\Tool;
+Tool::index();
---
+++ src/Abstarfish/Starfish/Tool.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2020/5/18
+ * Time: 23:08
+ */
+
+namespace Abstarfish\Starfish;
+class Tool
+{
+    public static function index()
+    {
+        echo  1;
+  }
+}
